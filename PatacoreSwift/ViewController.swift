aabd06d8578d78c57c98e7a3727c2fcd34f03adc
Johana <ejargoty@unicauca.edu.co>
2020-02-06 21:34:53 -0800

    Initial Commit

---
+++ PatacoreSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatacoreSwift
+//
+//  Created by Johana on 2/6/20.
+//  Copyright © 2020 Johana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
