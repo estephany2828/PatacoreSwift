1641166eb858d833cad072c4f7a3ea15b0931d10
Johana <ejargoty@unicauca.edu.co>
2020-02-08 21:19:20 -0800

    Estructura Inicial Login y TabBar

---
+++ PatacoreSwift/Controllers/SingInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SingInViewController.swift
+//  PatacoreSwift
+//
+//  Created by Johana on 2/8/20.
+//  Copyright © 2020 Johana. All rights reserved.
+//
+
+import UIKit
+
+class SingInViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func SingInBtn(_sender :  UIButton) {
+        
+        
+
+ /*let myTabBar = self.storyboard?.instantiateViewController (withIdentifier: "myTabBar") as! UITabBarController
+
+        self.navigationController?.pushViewController(myTabBar, animated: true)
+          
+         */
+        
+    }
+        /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
