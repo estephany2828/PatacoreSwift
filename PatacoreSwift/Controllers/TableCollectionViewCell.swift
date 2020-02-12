9de7a5a399869f2c6a262842ed7a57f5934ec9a2
Estephany2828 <ejargoty@unicauca.edu.co>
2020-02-11 20:00:21 -0500

    Vista order y table

---
+++ PatacoreSwift/Controllers/TableCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableCollectionViewCell.swift
+//  PatacoreSwift
+//
+//  Created by Telematica on 11/02/20.
+//  Copyright © 2020 Johana. All rights reserved.
+//
+
+import UIKit
+
+class TableCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelNumber: UILabel!
+}
