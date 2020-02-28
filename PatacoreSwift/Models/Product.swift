2780d48c5bfafb329af1174307273d1478ff46a8
Fernando Campo <juancamm@unicauca.edu.co>
2020-02-27 20:13:03 -0500

    Class Order

--- PatacoreSwift/Models/Product.swift
+++ PatacoreSwift/Models/Product.swift
@@ -39,3 +39,24 @@ class Product {
 }
 }
 
+class Order:Product{
+    var table:Int
+    var state:Int
+    var annotation:String
+    var quantity:Int
+    var date:String
+    var hour:String
+    
+    
+    init(product:Product, table:Int, state: Int, annotation:String, quantity:Int, date:String, hour:String){
+        self.table = table
+        self.state = state
+        self.annotation = annotation
+        self.quantity = quantity
+        self.date = date
+        self.hour = hour
+        
+        super.init(id:product.id, name: product.name, price: product.price, description: product.description, imag: product.imag)
+    }
+    
+}
