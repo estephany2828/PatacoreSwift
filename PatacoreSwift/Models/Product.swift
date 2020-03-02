33ae66ad0bc30dbd3692a193a7bfe3c09a4c982a
Johana <ejargoty@unicauca.edu.co>
2020-03-02 08:27:01 -0800

    PRICE INTEGER

--- PatacoreSwift/Models/Product.swift
+++ PatacoreSwift/Models/Product.swift
@@ -39,24 +39,4 @@ class Product {
 }
 }
 
-class Order:Product{
-    var table:Int
-    var state:Int
-    var annotation:String
-    var quantity:Int
-    var date:String
-    var hour:String
-    
-    
-    init(product:Product, table:Int, state: Int, annotation:String, quantity:Int, date:String, hour:String){
-        self.table = table
-        self.state = state
-        self.annotation = annotation
-        self.quantity = quantity
-        self.date = date
-        self.hour = hour
-        
-        super.init(id:product.id, name: product.name, price: product.price, description: product.description, imag: product.imag)
-    }
-    
-}
+
