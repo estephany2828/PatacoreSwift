748ce64dcda5144413e4d777c16dfd8aacca6f4e
Estephany2828 <ejargoty@unicauca.edu.co>
2020-03-03 15:15:56 -0500

    DB product test

--- PatacoreSwift/DataBase/DBHelper.swift
+++ PatacoreSwift/DataBase/DBHelper.swift
@@ -239,8 +239,6 @@ class DBHelper
         }
         sqlite3_finalize(queryStatement)
         return orders
-        
-        
     }
     
     func deleteOrder(table: Int, state: Int, product: String) {
@@ -250,7 +248,6 @@ class DBHelper
             sqlite3_bind_int(deleteStatement, 1, Int32(table))
             sqlite3_bind_int(deleteStatement, 2, Int32(state))
             sqlite3_bind_text(deleteStatement, 3, (product as NSString).utf8String, -1, nil)
-            
             
             if sqlite3_step(deleteStatement) == SQLITE_DONE {
                 print("Successfully deleted row.")
